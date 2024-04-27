8815234f236e3188e75d06f17add2afc09a45a19
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-04-27 13:18:00 +0530

    Refactor DonutChart component in admin-dashboard

--- src/components/dashboard/admin-dashboard/DonutChart.tsx
+++ src/components/dashboard/admin-dashboard/DonutChart.tsx
@@ -82,52 +82,53 @@ const ApiUsageChart: FC<ApiUsageChartType> = ({ chartData }) => {
 
   return (
     <div className='rounded-xl bg-slate-50/40 p-1.5 ring-1 ring-inset ring-input mt-10 w-full lg:w-1/2'>
-      <Card className='md:flex gap-10 z-10 rounded-lg p-4'>
-        <div>
-          <h3 className='text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong'>
-            Total API Usage by Category
-          </h3>
-          {/* Donut Chart Component */}
-          <DonutChart
-            className='mt-8'
-            data={categorizedChartData.map((item) => ({
-              name: item.name,
-              amount: item['Total API Requests'],
-            }))}
-            colors={categorizedChartData.map((item) => item.color)}
-            category='amount'
-            index='name'
-            valueFormatter={numberFormatter}
-            showTooltip={false}
-          />
-        </div>
-        <div>
-          {/* API Requests List */}
-          <p className='mt-8 flex items-center justify-between text-tremor-label text-tremor-content dark:text-dark-tremor-content'>
-            <span>Category</span>
-            <span>API Requests</span>
-          </p>
-          <List className='mt-2'>
-            {categorizedChartData.map((item) => (
-              <ListItem key={item.name} className='space-x-6'>
-                <div className='flex items-center space-x-2.5 truncate'>
-                  {/* Category Indicator */}
-                  <span
-                    className={cn('h-2.5 w-2.5 shrink-0 rounded-sm', `bg-${item.color}-500`)}
-                    aria-hidden={true}
-                  />
-                  {/* Category Name */}
-                  <span className='truncate dark:text-dark-tremor-content-emphasis'>{item.name}</span>
-                </div>
-                {/* API Requests Count */}
-                <div className='flex items-center space-x-2'>
-                  <span className='font-medium tabular-nums text-tremor-content-strong dark:text-dark-tremor-content-strong'>
-                    {item['Total API Requests']}
-                  </span>
-                </div>
-              </ListItem>
-            ))}
-          </List>
+      <Card className=' rounded-lg p-4 h-full'>
+        <h3 className='text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong'>
+          Total API Usage by Category
+        </h3>
+        <div className='block md:flex items-center justify-center gap-10 z-10 h-full'>
+          <div>
+            <DonutChart
+              className='w-full h-52'
+              data={categorizedChartData.map((item) => ({
+                name: item.name,
+                amount: item['Total API Requests'],
+              }))}
+              colors={categorizedChartData.map((item) => item.color)}
+              category='amount'
+              index='name'
+              valueFormatter={numberFormatter}
+              showTooltip={false}
+            />
+          </div>
+          <div>
+            {/* API Requests List */}
+            <p className='flex items-center justify-between text-tremor-label text-tremor-content dark:text-dark-tremor-content'>
+              <span>Category</span>
+              <span>API Requests</span>
+            </p>
+            <List className='mt-2'>
+              {categorizedChartData.map((item) => (
+                <ListItem key={item.name} className='space-x-6'>
+                  <div className='flex items-center space-x-2.5 truncate'>
+                    {/* Category Indicator */}
+                    <span
+                      className={cn('h-2.5 w-2.5 shrink-0 rounded-sm', `bg-${item.color}-500`)}
+                      aria-hidden={true}
+                    />
+                    {/* Category Name */}
+                    <span className='truncate dark:text-dark-tremor-content-emphasis'>{item.name}</span>
+                  </div>
+                  {/* API Requests Count */}
+                  <div className='flex items-center space-x-2'>
+                    <span className='font-medium tabular-nums text-tremor-content-strong dark:text-dark-tremor-content-strong'>
+                      {item['Total API Requests']}
+                    </span>
+                  </div>
+                </ListItem>
+              ))}
+            </List>
+          </div>
         </div>
       </Card>
     </div>
