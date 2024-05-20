7b9c7c11d5eafb3cdb93d778b6ef6253a800043a
Saddam Hossain <sksaddamh08@gmail.com>
2024-05-20 16:00:54 +0530

    landing pages file structure

--- src/app/(info)/layout.tsx
+++ src/app/(info)/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from '@/components/landing-page/Navbar';
-import Footer from '@/components/landing-page/Footer';
+import Navbar from '@/components/landing-pages/landing-page-1/Navbar';
+import Footer from '@/components/landing-pages/landing-page-1/Footer';
 
 // Custom layout for generic pages including Navbar and Footer.
 const layout = ({ children }: { children: React.ReactNode }) => {
---
+++ src/components/landing-pages/landing-page-1/index.tsx
@@ -0,0 +1,22 @@
+// This is the entry component for the landing page of the application.
+// It sequentially renders the primary sections of the landing page including Hero, Features, Product, Pricing, Faq, and Footer components.
+
+import Faq from './Faq';
+import Features from './Features';
+import Footer from './Footer';
+import Hero from './Hero';
+import Pricing from './pricing/Pricing';
+import Product from './Product';
+
+export default async function LandingPage1() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Product />
+      <Pricing />
+      <Faq />
+      <Footer />
+    </>
+  );
+}
