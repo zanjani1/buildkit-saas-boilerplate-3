2a8cd49bde2199dc57f4c174e670324227c45af0
Mohammad-Shahid-07 <112233075+Mohammad-Shahid-07@users.noreply.github.com>
2024-05-22 09:43:26 +0530

    fixed line height

--- src/components/landing-pages/landing-page-5/pricing/Pricing.tsx
+++ src/components/landing-pages/landing-page-5/pricing/Pricing.tsx
@@ -6,7 +6,7 @@ export default async function Pricing() {
   return (
     <div id='pricing' className='max-w-6xl mx-auto px-4 py-28'>
       <div className='flex  flex-col items-center justify-center'>
-        <h4 className='font-semibold text-white max-w-xl leading-[4.2rem] text-4xl max-sm:text-3xl text-center'>
+        <h4 className='font-semibold text-white max-w-md leading-[44px] text-4xl max-sm:text-3xl text-center'>
           Start making
           <span className='text-lp2-primary'> smarter decisions</span>, Choose a plan
         </h4>
