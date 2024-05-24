dd9fa7d6a3b1da12cc5fdb295e94f1fe6c64b1e3
Vishesh <visheshdpanchal@gmail.com>
2024-05-24 21:21:10 +0530

    feat:landing page 8 completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 // This is the entry component for the landing page of the application.
 // It sequentially renders the primary sections of the landing page including Hero, Features, Product, Pricing, Faq, and Footer components.
 
-import LandingPage1 from '@/components/landing-pages/landing-page-1';
+// import LandingPage1 from '@/components/landing-pages/landing-page-1';
 // import LandingPage3 from '@/components/landing-pages/landing-page-3';
 // import LandingPage4 from '@/components/landing-pages/landing-page-4';
+import LandingPage8 from '@/components/landing-pages/landing-page-8';
 
 export default async function Home() {
   return (
     <>
-      <LandingPage1 />
+      {/* <LandingPage1 /> */}
       {/* <LandingPage3 /> */}
       {/* <LandingPage4 /> */}
+      <LandingPage8 />
     </>
   );
 }
