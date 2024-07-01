369b5708f64f1d46ee275a2419179b9f2129b965
Saddam Hossain <sksaddamh08@gmail.com>
2024-07-01 16:58:10 +0530

    aupabase auth depricated fix

--- src/components/auth/GoogleAuth.tsx
+++ src/components/auth/GoogleAuth.tsx
@@ -5,39 +5,31 @@
 'use client';
 
 import { supabaseBrowserClient } from '@/utils/supabase/client';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Button } from '@/components/ui/button';
+import { FaGoogle } from 'react-icons/fa6';
 
 export default function GoogleAuth() {
-  const supabase = supabaseBrowserClient();
+  const handleGoogleAuth = async () => {
+    const supabase = supabaseBrowserClient();
 
-  // Ensure the redirect URL is configured correctly in the Supabase project settings.
-  // Incorrect configuration can lead to failed authentication attempts or security vulnerabilities.
-  const redirectUrl = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
+    // Ensure the redirect URL is configured correctly in the Supabase project settings.
+    // Incorrect configuration can lead to failed authentication attempts or security vulnerabilities.
+    const redirectUrl = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
+
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: redirectUrl,
+      },
+    });
+  };
 
   return (
     <div className='w-full mt-4'>
-      <Auth
-        supabaseClient={supabase}
-        onlyThirdPartyProviders={true}
-        providers={['google']}
-        redirectTo={redirectUrl}
-        appearance={{
-          theme: ThemeSupa,
-          variables: {
-            default: {
-              colors: {
-                defaultButtonText: 'white',
-                defaultButtonBackground: '#2078FF',
-                defaultButtonBackgroundHover: '#2078ffeb',
-              },
-              radii: {
-                borderRadiusButton: '6px',
-              },
-            },
-          },
-        }}
-      />
+      <Button size='lg' onClick={handleGoogleAuth} className='w-full bg-[#2078FF] gap-2'>
+        <FaGoogle size={17} />
+        <span>Sign in with Google</span>
+      </Button>
     </div>
   );
 }
