d9073373e27f03f561a880cec6ef5f93de24d7ba
Saddam Hossain <sksaddamh08@gmail.com>
2024-08-29 14:51:48 +0530

    sample copy update for lp 11

--- src/components/landing-pages/landing-page-11/feedback/content.ts
+++ src/components/landing-pages/landing-page-11/feedback/content.ts
@@ -8,18 +8,6 @@ const feedbacks: feedbacks[] = [
   {
     content:
       'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
-    author: 'FIRST-1',
-    role: 'Founder',
-  },
-  {
-    content:
-      'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
-    author: 'FIRST-2',
-    role: 'Founder',
-  },
-  {
-    content:
-      'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
     author: 'Samuel Jack',
     role: 'Founder',
   },
@@ -92,13 +80,25 @@ const feedbacks: feedbacks[] = [
   {
     content:
       'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
-    author: 'LAST-1',
+    author: 'Samuel Jack',
     role: 'Founder',
   },
   {
     content:
       'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
-    author: 'LAST-2',
+    author: 'Samuel Jack',
+    role: 'Founder',
+  },
+  {
+    content:
+      'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
+    author: 'Samuel Jack',
+    role: 'Founder',
+  },
+  {
+    content:
+      'Builderkit takes care of everything from authentication to payments, get your production-ready app within hours.',
+    author: 'Samuel Jack',
     role: 'Founder',
   },
 ];
